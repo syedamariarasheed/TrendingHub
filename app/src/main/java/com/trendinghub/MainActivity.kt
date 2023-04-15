eb6bbffa946386f3480e5be16dfdcfa13fa7be21
Syeda Maria Rasheed <syedamariarasheed@gmail.com>
2023-04-15 22:25:37 +0500

    refactor main activity remove default code

--- app/src/main/java/com/trendinghub/MainActivity.kt
+++ app/src/main/java/com/trendinghub/MainActivity.kt
@@ -3,12 +3,7 @@ package com.trendinghub
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.trendinghub.ui.TrendingScreen
 import com.trendinghub.ui.common.theme.TrendingHubTheme
@@ -18,27 +13,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TrendingHubTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    TrendingScreen()
-                }
+                TrendingScreen()
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TrendingHubTheme {
-        Greeting("Android")
+        TrendingScreen()
     }
 }
