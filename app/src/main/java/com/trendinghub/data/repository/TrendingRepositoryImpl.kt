fd35fc5fed95fea34ad79f7c3af008b0e59616ec
Syeda Maria Rasheed <syedamariarasheed@gmail.com>
2024-03-12 13:09:32 +0500

    map flow

--- app/src/main/java/com/trendinghub/data/repository/TrendingRepositoryImpl.kt
+++ app/src/main/java/com/trendinghub/data/repository/TrendingRepositoryImpl.kt
@@ -7,23 +7,24 @@ import com.trendinghub.domain.model.TrendingData
 import com.trendinghub.domain.repository.TrendingRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class TrendingRepositoryImpl @Inject constructor(
     private val trendingRemoteDataSource: TrendingRemoteDataSource
 ) : TrendingRepository {
-    override fun fetchTrendingList(): Flow<ResultState<List<TrendingData>>> = flow {
-        trendingRemoteDataSource.fetchTrendingList().collect {
+    override fun fetchTrendingList(): Flow<ResultState<List<TrendingData>>> =
+        trendingRemoteDataSource.fetchTrendingList().map {
             when (it) {
                 is ResultState.Success -> {
-                    val result = it.data.items.map { it.toTrendingData() }
-                    emit(ResultState.Success(result))
+                    ResultState.Success(it.data.items.map { it.toTrendingData() })
                 }
 
                 is ResultState.Error -> {
-                    emit(it)
+                    it
                 }
             }
         }
-    }
+
 }
